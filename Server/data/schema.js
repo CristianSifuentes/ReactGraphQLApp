5b741219b64b92d1b957b50a08092cf21672139e
Cristian SC <sifuentescovarrubiasitsl@gmail.com>
2019-03-24 18:14:25 -0600

    CSC 2019/03/24: configuración inicial de apollo server express

--- Server/data/schema.js
+++ Server/data/schema.js
@@ -1,18 +1,3 @@
-// import {buildSchema} from "graphql";
-
-// // const schema = buildSchema(`
-  
-// //   `);
-
-// export default schema;
-
-
-import { resolvers } from './resolvers';
 import { importSchema } from 'graphql-import';
-import { makeExecutableSchema  } from 'graphql-tools';
-
 const typeDefs = importSchema('data/schema.graphql');
-
-const schema = makeExecutableSchema({typeDefs, resolvers});
-
-export { schema };
+export { typeDefs };
--- Server/index.js
+++ Server/index.js
@@ -1,62 +1,15 @@
-// var express = require('express');
-// var graphqlHTTP = require('express-graphql');
-// var { buildSchema } = require('graphql');
-
-// // Construct a schema, using GraphQL schema language
-// var schema = buildSchema(`
-//   type Query {
-//     hello: String
-//   }
-// `);
-
-// // The root provides a resolver function for each API endpoint
-// var root = {
-//   hello: () => {
-//     return 'Hello world!';
-//   },
-// };
-
-// var app = express();
-// app.use('/graphql', graphqlHTTP({
-//   schema: schema,
-//   rootValue: root,
-//   graphiql: true,
-// }));
-// app.listen(4000);
-// console.log('Running a GraphQL API server at localhost:4000/graphql');
-
-
-
-
  import express from 'express';
 
  //importar
- import graphqlHTTP  from 'express-graphql';
- import { schema } from './data/schema';
+ import { ApolloServer }  from 'apollo-server-express';
+ import { typeDefs } from './data/schema';
+ import { resolvers  } from './data/resolvers';
 
  const app = express();
+ const server = new ApolloServer({ typeDefs, resolvers });
 
- app.get('/', (req, res) => {
-   res.send('Todo listo');
+ server.applyMiddleware({app});
+
+ app.listen({port: 4000}, () => {
+   console.log(`El servidor está corriendo http:localhost:4000${server.graphqlPath}`);
  });
-
-
-
-app.use('/graphql', graphqlHTTP({
-  //Que schema va a utilizar
-  schema: schema, 
-  //y utilizar graphical
-  graphiql : true
-
-}));
-
- app.listen(8092, () => console.log('El servidor está funcionando'));
-
-// import http from 'http';
-
-// http.createServer((req, res) => {
-//   res.writeHead(200, {'Content-Type': 'text/plain'});
-//   res.end('Hello World\n');
-// }).listen(1337, '127.0.0.1');
-
-// console.log('Server running at http://127.0.0.1:1337/');
